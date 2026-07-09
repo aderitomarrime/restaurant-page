36b7a86b9ca25e0064994a69bc3f5e375cc39f5a
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2026-07-09 21:43:43 +0200

    Add styles to the header and body

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const menuButton = document.querySelector('button:nth-child(2)');
 const aboutButton = document.querySelector('button:nth-child(3)');
 let divContent = document.querySelector('#content');
 
+import "./styles/header.css"
+
 import {makeHome} from "./home.js";
 import {cards, makeMenu} from "./menu.js";
 import {makeAbout} from "./about.js";
--- webpack.config.js
+++ webpack.config.js
@@ -21,14 +21,18 @@ export default {
   ],
   module: {
     rules: [
-        {
-          test: /\.html$/i,
-          use: ["html-loader"],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
-        },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
